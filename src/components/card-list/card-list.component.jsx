76f803a86d039944441e05896b0661b0ff87ee6a
Mark Ghaida <markghaida@gmail.com>
2021-05-14 13:10:08 -0400

    added the card list mapping statement inside of card list compnonent and removed it from App.js

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,5 +3,9 @@ import './card-list.styles.css';
 
 export const CardList = props => {
     // console.log(props)
-    return <div>{props.children}</div>;
+    return (<div className='card-list'>        
+        {props.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+        </div>);
 } 
