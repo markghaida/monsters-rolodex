1608f05d0dfe82c54680f3ae08fa0b2c8317ecaf
Mark Ghaida <markghaida@gmail.com>
2021-05-14 16:00:32 -0400

    added search bar with filter functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -17,16 +18,27 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({monsters: users}));
   }
+//  I need to create a function that is triggered everytime onChange is triggered
+//  this new function should essentially take in the monsters array and then apply a filter function to it that reflect the onChange e.target.value
+
+
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())     
+    )
     return (
       <div className='App'>
-        <input type='search' placeholder='search for monsters'/> 
-        <CardList monsters={this.state.monsters}/>
-      </div>
-    )  
+        <input 
+          type='search' 
+          placeholder='search for monsters' 
+          onChange={ e => this.filterMonsters(e.target.value)} 
+          />  
+          <CardList monsters={filteredMonsters}/>
+          </div>
+    )
+
   }
-
 }
-
 export default App;
