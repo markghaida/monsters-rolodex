b4b09e77d0fdb009e6e55091114a2f41b1cabca5
Mark Ghaida <markghaida@gmail.com>
2021-05-14 14:14:01 -0400

    added search component

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <input type='search' placeholder='search for monsters'/> 
         <CardList monsters={this.state.monsters}/>
       </div>
     )  
